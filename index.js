cabec3a0a4693359497dc124669f40d262cbdc9b
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-04-12 01:20:51 -0500

    Adds more functions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 const bodyParser = require('body-parser')
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const axios = require('axios')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -18,9 +19,13 @@ client.on('ready', () => {
 client.on('message', message => {
   const tokens = message.content.split(' ')
 
-  if (tokens[0] === '!setup' && message.member.hasPermission('ADMINISTRATOR')) {
+  if (
+    tokens[0] === '!setup' &&
+    tokens[1] !== null &&
+    message.member.hasPermission('ADMINISTRATOR')
+  ) {
     message.guild.roles.find(role => {
-      if (role.name === '2. Professors') {
+      if (role.name === '2. Instructors') {
         message.guild.members
           .find(member => member.id === message.author.id)
           .addRole(role.id)
@@ -39,38 +44,94 @@ client.on('message', message => {
           .addRole(role.id)
       }
     })
+
+    message.guild.channels.forEach(channel => {
+      if (channel.name === 'command-book') {
+        client.channels
+          .get(channel.id)
+          .send(
+            '**Command List**\n```!openoffice - Allows students to access office channels```\n```!closeoffice - Closes the office from students```\n```!next - Automatically moves the next person in line in the waiting room channel```\nYou can also run these commands from our web portal! (http://academe.space)'
+          )
+      }
+    })
+    const verification = tokens[1]
+    const serverID = message.guild.id
+    const ownerID = message.author.id
+    axios.get('verifyserver').then(res => {
+      if (res.success) {
+        message.reply(
+          'Your server is now connected to our platform. **Please go back to the website and click "Create Server" to complete the process!**'
+        )
+      } else {
+        message.reply(
+          'Server setup has failed! Code may be expired. Please try again.'
+        )
+      }
+    })
   }
 
   if (
-    message.content == '!taopen' &&
+    message.content == '!openoffice' &&
     message.member.hasPermission('ADMINISTRATOR')
   ) {
-    message.guild
-      .createChannel(
-        'TA Office (' + message.member.user.username + ')',
-        'category'
-      )
-      .then(category => {
-        message.guild
-          .createChannel('ta-text', 'text')
-          .then(channel => channel.setParent(category.id))
-        message.guild
-          .createChannel('ta-voice', 'voice')
-          .then(channel => channel.setParent(category.id))
-      })
+    message.guild.roles.forEach(role => {
+      if (role.name === '1. Students') {
+        message.guild.channels.forEach(channel => {
+          if (channel.name === "Professor's Office") {
+            channel.overwritePermissions(role, {
+              VIEW_CHANNEL: true,
+              SEND_MESSAGES: true
+            })
+            message.reply('Office Hours are now in Session!')
+          }
+        })
+      }
+    })
   }
 
   if (
-    message.content == '!taclose' &&
+    message.content == '!closeoffice' &&
+    message.member.hasPermission('ADMINISTRATOR')
+  ) {
+    message.guild.roles.forEach(role => {
+      if (role.name === '1. Students') {
+        message.guild.channels.forEach(channel => {
+          if (channel.name === "Instructor's Office") {
+            channel.overwritePermissions(role, {
+              VIEW_CHANNEL: false,
+              SEND_MESSAGES: false
+            })
+            message.reply('Office Hours are now closed!')
+          }
+        })
+      }
+    })
+  }
+
+  if (
+    message.content == '!next' &&
     message.member.hasPermission('ADMINISTRATOR')
   ) {
     message.guild.channels.forEach(channel => {
-      if (
-        channel.name === 'TA Office (' + message.member.user.username + ')' ||
-        channel.name === 'ta-text' ||
-        channel.name === 'ta-voice'
-      ) {
-        channel.delete()
+      if (channel.name === 'instructor-voice') {
+        let officeLine = []
+        message.guild.members.forEach(member => {
+          if (member.voiceChannel != null || member.voiceChannel === '') {
+            if (!member.hasPermission('ADMINISTRATOR')) {
+              officeLine.push(member)
+            }
+          }
+        })
+
+        if (officeLine == 0) {
+          message.reply('The line is empty!')
+        } else {
+          const student =
+            officeLine[Math.floor(Math.random() * officeLine.length)]
+
+          student.setVoiceChannel(channel)
+          message.reply('@' + student.displayName + ', you are up!')
+        }
       }
     })
   }
@@ -132,6 +193,46 @@ app.get('/officehours/open/:server', (req, res) => {
 
 app.get('/officehours/close/:server', (req, res) => {
   let server = client.guilds.find(s => s.id === req.params.server)
+
+  res.send('Success')
+})
+
+app.get('/next/:server', (req, res) => {
+  let server = client.guilds.find(s => s.id === req.params.server)
+
+  server.channels.forEach(channel => {
+    if (channel.name === 'instructor-voice') {
+      let officeLine = []
+      server.members.forEach(member => {
+        if (member.voiceChannel != null || member.voiceChannel === '') {
+          if (!member.hasPermission('ADMINISTRATOR')) {
+            officeLine.push(member)
+          }
+        }
+      })
+
+      if (officeLine == 0) {
+        server.channels.forEach(channel => {
+          if (channel.name === 'bot-spam') {
+            client.channels.get(channel.id).send('The line is empty!')
+          }
+        })
+      } else {
+        const student =
+          officeLine[Math.floor(Math.random() * officeLine.length)]
+
+        student.setVoiceChannel(channel)
+        server.channels.forEach(channel => {
+          if (channel.name === 'bot-spam') {
+            client.channels
+              .get(channel.id)
+              .send("@' + student.displayName + ', you are up!")
+          }
+        })
+        message.reply()
+      }
+    }
+  })
 
   res.send('Success')
 })
